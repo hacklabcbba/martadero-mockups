c41b64bb9859d423ac915c2f4c6b10287e6f07a5
Marcelo Rodríguez Claros <marcelo.rodriguezc@genso.com.bo>
2015-09-30 18:55:06 -0400

    Primeros HTMLs - CSS - JS

---
+++ HTML-CSS-JS/Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  // Project configuration.
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          sourceMap: true,
+          sourceMapFilename: 'css/style.css.map', // where file is generated and located
+          sourceMapURL: '/martadero/css/style.css.map', // the complete url and filename put in the compiled css file
+          sourceMapRootpath: '/martadero/' // adds this path onto the sourcemap filename and less file paths
+        },
+        files: {
+          'css/styles.css': 'css/styles.less'
+        }
+      }
+    },
+    watch: {
+      less: {
+        files: ['css/styles.less'],
+        tasks: ['less:development'],
+        options: {
+          spawn: true
+        }
+      }
+    }
+  });
+};
+
