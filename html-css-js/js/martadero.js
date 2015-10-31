84948f38813eb4f74a50b6758baa4b68829f6a4c
Marcelo Rodríguez Claros <marcelo.rodriguezc@genso.com.bo>
2015-10-31 13:02:47 -0400

    home page

--- html-css-js/js/martadero.js
+++ html-css-js/js/martadero.js
@@ -8,3 +8,17 @@ $(document).ready(function(){
         $('.gs-slidenav-menu').removeClass('close-menu');
     } )
 });
+
+
+//$(document).ready(function(){
+//    var $event = $(".events .event.feature:nth-child(2n+2)").next(".event").addClass("eurke");
+//    $event.each(function(){
+//        var $eventPosition = $(this).position().top;
+//        console.log($(this).position().top);
+//        //$(this).css({
+//        //    position: 'absolute',
+//        //    top: $eventPosition - 275 +'px'
+//        //});
+//    });
+//});
+
